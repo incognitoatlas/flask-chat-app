a60b05037c2cf113ac6152e732e8b68144a0cdd5
Michael <90201909+incognitoatlas@users.noreply.github.com>
2021-12-09 13:42:59 +0000

    a

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request
-from datetime import datetime
-from replit import db
+from replit import # db
 from flask_cors import CORS
 import logging
 from better_profanity import profanity
-  
+
 app = Flask('app')
 CORS(app)
-db['users'] = 0
-db['messages'] = []
+# db['users'] = 0
+# db['messages'] = []
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
@@ -19,30 +18,30 @@ def web_index():
     return render_template('index.html')
   else:
     msg_form = request.form['msg_content']
-    db['messages'].append(msg_form)
+    # db['messages'].append(msg_form)
     return render_template('index.html')
       
 @app.route('/append/<item>')
 def append_item(item):
   if item.startswith("!"):
     if item == '!clear':
-      db['messages'] = []
+      # db['messages'] = []
       return 'True'
   if profanity.contains_profanity(item):
     censored = profanity.censor(item)
-    db['messages'].append(censored)
+    # db['messages'].append(censored)
     return 'True'
   else:
-    db['messages'].append(item)
+    # db['messages'].append(item)
   return 'True'
 
 @app.route('/user/<_bool>')
 def user_index(_bool):
   if _bool == 'add':
-    db['users'] += 1
+    # db['users'] += 1
     return ''
   elif _bool == 'sub':
-    db['users'] -= 1
+    # db['users'] -= 1
     return ''
   else:
     return ''
@@ -50,8 +49,8 @@ def user_index(_bool):
 @app.route('/fetch_msg')
 def return_messages():
   return {
-    'result': list(db['messages']),
-    'users_active': int(db['users'])
+    'result': list(# db['messages']),
+    'users_active': int(# db['users'])
   }
 
 app.run(host='0.0.0.0', port=8080)
