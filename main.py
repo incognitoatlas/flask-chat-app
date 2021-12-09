55dc956978fb6613e55e00d738f45d15e96e7f54
Michael <90201909+incognitoatlas@users.noreply.github.com>
2021-12-09 03:44:51 +0000

    a

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 from replit import db
+from flask_cors import CORS
+import logging
+from better_profanity import profanity
+  
 app = Flask('app')
+CORS(app)
+db['users'] = 0
+db['messages'] = []
 
-@app.route('/')
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+@app.route('/', methods=["GET", 'POST'])
 def web_index():
-  return render_template('index.html')
-
-@app.route('/api/time')
-def api_timeapi():
-  now = datetime.now()
-  return {
-    "time-raw": datetime.now(),
-    "year": now.strftime("%Y"),
-    "month": now.strftime("%m"),
-    "day": now.strftime("%d")
-  }
-
-@app.route('/api/createacc')
-def _createacc():
-  _user = request.args.get("username")
-  _pass = request.args.get("password")
-  try:
-    if isinstance(db[_user], str):
-      return {"result": "Username already taken.."}
-    else: 
-      db[_user] = _pass
-      return {"result": "Success! Account Created!"}
-  except:
-    db[_user] = _pass
-    return {"result": "Success! Account Created!"}
-    
-@app.route('/api/login')
-def acclogin():
-  _user = request.args.get("username")
-  _pass = request.args.get("password")
-  try:
-    db[_user] == _pass
-  except:
-    return {"result": "Incorrect username or password.."}
+  if request.method == 'GET':
+    return render_template('index.html')
   else:
-    if db[_user] == _pass:
-      return {"result": f"Success! Welcome user {_user}"}
-    else:
-      return {"result": "Incorrect username or password.."}
+    msg_form = request.form['msg_content']
+    db['messages'].append(msg_form)
+    return render_template('index.html')
+      
+@app.route('/append/<item>')
+def append_item(item):
+  if item.startswith("!"):
+    if item == '!clear':
+      db['messages'] = []
+      return 'True'
+  if profanity.contains_profanity(item):
+    censored = profanity.censor(item)
+    db['messages'].append(censored)
+    return 'True'
+  else:
+    db['messages'].append(item)
+  return 'True'
 
+@app.route('/user/<_bool>')
+def user_index(_bool):
+  if _bool == 'add':
+    db['users'] += 1
+    return ''
+  elif _bool == 'sub':
+    db['users'] -= 1
+    return ''
+  else:
+    return ''
 
+@app.route('/fetch_msg')
+def return_messages():
+  return {
+    'result': list(db['messages']),
+    'users_active': int(db['users'])
+  }
 
 app.run(host='0.0.0.0', port=8080)
--- static/app.jsx
+++ static/app.jsx
@@ -2,62 +2,110 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: 'no status',
-      usernameinput: null,
-      passwordinput: null
+      messages: [],
+      mapped_list: null,
+      input: '',
+      name: 'unnamed user',
+      users: 0
+    };
+    this.fetch_messages = this.fetch_messages.bind(this);
+    this.updateMsg = this.updateMsg.bind(this);
+    this.updateName = this.updateName.bind(this);
+    this.handleForm = this.handleForm.bind(this);
+    this._handleKeyDown = this._handleKeyDown.bind(this);
+    this.unload = this.unload.bind(this)
+  }
+
+  updateName(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  updateMsg(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  fetch_messages() {
+    var messageList;
+    fetch(`https://chat.cortemic.repl.co/fetch_msg`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({users: data.users_active});
+        if ( data.result.length != this.state.messages.length ) {
+          this.setState({messages: data.result});
+          this.setState({mapped_list: this.state.messages.map((item) => <li>{item}</li>)});
+          var objDiv = document.getElementById("msg_div");
+          objDiv.scrollTop = objDiv.scrollHeight;
+        }
+      })
+  }
+
+  handleForm() {
+    if (this.state.input != '') {
+      if (this.state.input.startsWith('!')) {
+        fetch(`https://chat.cortemic.repl.co/append/${this.state.input}`)
+        .then(response => response.json());
+      } else {
+        let merged = this.state.name + ': ' + this.state.input
+        fetch(`https://chat.cortemic.repl.co/append/${merged}`)
+        .then(response => response.json());
+      }
+    document.getElementById('msginput').value = '';
+  }
+  this.setState({
+    input: ''
+  });
+  }
+
+  componentDidMount() {
+    fetch('https://chat.cortemic.repl.co/user/add');
+    window.addEventListener("beforeunload", (ev) => {
+      ev.preventDefault();
+      return this.unload();
+    });
+    setInterval(this.fetch_messages, 500);
+    document.addEventListener("keydown", this._handleKeyDown);
+  }
+
+  unload() {
+    fetch('https://chat.cortemic.repl.co/user/sub');
+  }
+  
+
+  _handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      if (this.state.input != '') {
+    if (this.state.input.startsWith('!')) {
+      fetch(`https://chat.cortemic.repl.co/append/${this.state.input}`);
+    } else {
+    let merged = this.state.name + ': ' + this.state.input
+    fetch(`https://chat.cortemic.repl.co/append/${merged}`);
     }
-    this.handleCreateAccount = this.handleCreateAccount.bind(this);
-    this.updateUser = this.updateUser.bind(this);
-    this.updatePass = this.updatePass.bind(this);
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handleCreateAccount = this.handleCreateAccount.bind(this);
+    document.getElementById('msginput').value = '';
   }
-
-  updateUser(event) {
-    this.setState({
-      usernameinput: event.target.value
-    });
+  this.setState({
+    input: ''
+  });
+    }
   }
-
-  updatePass(event) {
-    this.setState({
-      passwordinput: event.target.value
-    });
-  }
-
-  handleCreateAccount() {
-    fetch(`https://reactflask.cortemic.repl.co/api/createacc?username=${this.state.usernameinput}&password=${this.state.passwordinput}`)
-      .then(response => response.json())
-      .then(data => this.setState({
-        status: data.result
-      }));
-  }
-
-  handleLogin() {
-    fetch(`https://reactflask.cortemic.repl.co/api/login?username=${this.state.usernameinput}&password=${this.state.passwordinput}`)
-      .then(response => response.json())
-      .then(data => this.setState({
-        status: data.result
-      }));
-  }
-
 
   render() {
     return(
       <div>
-        <label for="user">Username:</label>
-        <br />
-        <input type="text" id="usernameinput" name="user" onChange={this.updateUser}></input>
-        <br />
-        <label for="pass">Password:</label>
-        <br />
-        <input type="text" id="passwordinput" name="pass" onChange={this.updatePass}></input>
-        <br />
-        <button id="login" onClick={this.handleLogin}>login</button>
-        <button id="create" onClick={this.handleCreateAccount}>create acc</button>
-        <br />
-        <p id="status">{this.state.status}</p>
-      </div>);
+      <p id="users">Users Connected: {this.state.users}</p>
+      <div id="msg_div">
+        <ul>
+          {this.state.mapped_list}
+        </ul>
+      </div>
+          <input onChange={this.updateName} placeholder="Name"/>
+          <input onChange={this.updateMsg} placeholder="Message" id="msginput" autocomplete="off" onKeyPress={this._handleKeyDown}/>
+          <button onClick={this.handleForm}>Send</button>
+      </div>
+    );
   }
 }
 
